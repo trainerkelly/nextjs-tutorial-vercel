f90d5d49dce3f8f06cc02bb3595cf9100939ec88
Kelly Mulry <trainerkelly@live.com>
2024-10-16 07:19:50 -0400

    i dunno i added error handling and accessibility stuff and now stuff is broken and i don't know why

--- nextjs-dashboard/app/lib/data.ts
+++ nextjs-dashboard/app/lib/data.ts
@@ -158,7 +158,7 @@ export async function fetchInvoiceById(id: string) {
       amount: invoice.amount / 100,
     }));
 
-    return invoice[0];
+    console.log(invoice); // Invoice is an empty array []    return invoice[0];
   } catch (error) {
     console.error("Database Error:", error);
     throw new Error("Failed to fetch invoice.");
